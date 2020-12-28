d89999842c94741c76c114e48c5c2ef7f423735b
Cong Dao <congdaovan94@gmail.com>
2020-12-28 01:52:53 -0500

    Add two modes of cli app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 #!/usr/bin/env node
 import program from 'commander';
+import readline from 'readline';
 import SXS from './SExpression';
+
+function calculate(value: string) {
+  try {
+    const result = SXS(value);
+    console.log(result);
+  } catch (error) {
+    console.log(`sxs: ${error.getMessage()}`);
+  }
+}
+
+//https://stackoverflow.com/questions/24464404/how-to-readline-infinitely-in-node-js
+function interactMode() {
+  console.log('Type "exit" or Ctrl-D (i.e. EOF) to exit');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const prompt = () => {
+    rl.question('>>> ', function (answer) {
+      if (answer.toLowerCase() == 'exit') return rl.close();
+      calculate(answer);
+      prompt();
+    });
+  };
+  prompt();
+}
 
 export function main(argv: string[]): void {
   program
     .version('0.0.1')
-    .usage("[global options] command")
+    .usage(`[expression]\n  Examples:\n\t- sxs 233 \n\t- sxs "(add 2 3)' `)
     .description('An CLI app for S-Expressing')
     .parse(argv);
 
-  const value = argv[2];
-  console.log(value);
+  if (argv.length > 2) {
+    calculate(argv[2]);
+  } else {
+    interactMode();
+  }
 }
 
 main(process.argv);
